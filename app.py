8bc041b56cf33e9fdc0334c41a54396f4f2c0f15
S T <itschibits@gmail.com>
2021-04-23 19:44:49 -0400

    wrote tests for scoring word

--- app.py
+++ app.py
@@ -20,11 +20,28 @@ def homepage():
 @app.route("/api/new-game", methods=["POST", "GET"])
 def new_game():
     """Start a new game and return JSON: {game_id, board}."""
-
-
     # get a unique id for the board we're creating
     game_id = str(uuid4())
     game = BoggleGame()
     games[game_id] = game
 
-    return jsonify({"gameId": game_id, "board": game})
+    return jsonify({"gameId": game_id, "board": game.board})
+
+
+@app.route("/api/score-word", methods=["POST"])
+def score_word():
+    # if not a word: {result: "not-word"}
+    # if not on board: {result: "not-on-board"}
+    # otherwise, {result: "ok"}
+    game_id = request.json["gameId"]
+    game = games[game_id]
+    word = request.json["word"].upper()
+    breakpoint()
+
+    if not game.is_word_in_word_list(word):
+        return jsonify({"result": "not-word"})
+
+    if not game.check_word_on_board(word):
+        return jsonify({"result": "not-on-board"})
+
+    return jsonify({"result": "ok"})
--- test_app.py
+++ test_app.py
@@ -33,10 +33,52 @@ class BoggleAppTestCase(TestCase):
 
         with self.client as client:
             response = client.get('/api/new-game')
+            json_dict = response.get_json()
+            board = json_dict['board']
+
             print(response)
             # write a test for this route
 
-            self.assertIn("gameId", id_and_board)
-            #self.assertTrue(isinstance(games['board'], list), True)
-            
+            self.assertIn('gameId', json_dict)
+            self.assertTrue(isinstance(board, list), True)
 
+    def test_score_word(self):
+        """Test scoring a played word"""
+        with self.client as client:
+            response = client.get('/api/new-game')
+            json_dict = response.get_json()
+            game_id = json_dict['gameId']
+            game = games[game_id]
+            game.board[0] = ["E", "U", "S", "S", "O"]
+            game.board[1] = ["A", "E", "R", "R", "U"]
+            game.board[2] = ["N", "A", "O", "E", "T"]
+            game.board[3] = ["I", "E", "C", "Y", "M"]
+            game.board[4] = ["T", "R", "E", "E", "Z"]
+
+            response = client.post(
+                    '/api/score-word',
+                    json={"word": "tree", "gameId": game_id}
+                    )
+
+            self.assertEqual(response.get_json(), {"result": "ok"})
+
+            response = client.post(
+                    '/api/score-word',
+                    json={"word": "sort", "gameId": game_id}
+                    )
+
+            self.assertEqual(response.get_json(), {"result": "ok"})
+
+            response = client.post(
+                    '/api/score-word',
+                    json={"word": "zonally", "gameId": game_id}
+                    )
+
+            self.assertEqual(response.get_json(), {"result": "not-on-board"})
+
+            response = client.post(
+                    '/api/score-word',
+                    json={"word": "EUSSOURREAN", "gameId": game_id}
+                    )
+
+            self.assertEqual(response.get_json(), {"result": "not-word"})
